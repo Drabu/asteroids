76e13e6632efa151083db4a8aa2a59b9cd70f666
drabu <burhan.drabu@shadowfax.in>
2024-06-08 18:10:48 +0530

    feat(l2): add asteriods scattered randomly

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -22,12 +24,40 @@ class MyApp extends StatelessWidget {
 
 class MouseTracker extends StatefulWidget {
   const MouseTracker({super.key});
+
   @override
   MouseTrackerState createState() => MouseTrackerState();
 }
 
 class MouseTrackerState extends State<MouseTracker> {
-  Offset _mousePosition = Offset.zero;
+  Offset _mousePosition = Offset(50, 50); // Set initial position to be visible
+  final List<Particle> _particles = [];
+  bool _particlesGenerated = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (!_particlesGenerated) {
+      _generateParticles();
+      _particlesGenerated = true;
+    }
+  }
+
+  void _generateParticles() {
+    final random = Random();
+    const int particleCount = 30;
+    const double minSize = 5.0;
+    const double maxSize = 30.0;
+
+    for (int i = 0; i < particleCount; i++) {
+      double size = minSize + random.nextDouble() * (maxSize - minSize);
+      Offset position = Offset(
+        random.nextDouble() * MediaQuery.of(context).size.width,
+        random.nextDouble() * MediaQuery.of(context).size.height,
+      );
+      _particles.add(Particle(position, size));
+    }
+  }
 
   void _updateMousePosition(PointerEvent details) {
     setState(() {
@@ -43,7 +73,7 @@ class MouseTrackerState extends State<MouseTracker> {
         child: MouseRegion(
           onHover: _updateMousePosition,
           child: CustomPaint(
-            painter: BallPainter(_mousePosition),
+            painter: BallPainter(_mousePosition, _particles),
             child: Container(),
           ),
         ),
@@ -52,18 +82,36 @@ class MouseTrackerState extends State<MouseTracker> {
   }
 }
 
+class Particle {
+  final Offset position;
+  final double size;
+
+  Particle(this.position, this.size);
+}
+
 class BallPainter extends CustomPainter {
   final Offset position;
+  final List<Particle> particles;
 
-  BallPainter(this.position);
+  BallPainter(this.position, this.particles);
 
   @override
   void paint(Canvas canvas, Size size) {
-    final paint = Paint()
+    final ballPaint = Paint()
       ..color = Colors.white
       ..style = PaintingStyle.fill;
 
-    canvas.drawCircle(position, 10.0, paint);
+    final particlePaint = Paint()
+      ..color = Colors.red
+      ..style = PaintingStyle.fill;
+
+    // Draw particles
+    for (final particle in particles) {
+      canvas.drawCircle(particle.position, particle.size, particlePaint);
+    }
+
+    // Draw tracking ball
+    canvas.drawCircle(position, 10.0, ballPaint);
   }
 
   @override
